000a4cd0a3b12ad4ab694cd3ca0819f9df2748fb
secw01f <16842677+secw01f@users.noreply.github.com>
2021-09-20 15:11:16 -0400

    fixed directory issue

--- gs-payload.py
+++ gs-payload.py
@@ -79,8 +79,8 @@ def main():
         elif o in ('-u', '--url'):
             Url = a
 
-    if os.path.exists('/opt/gs-payload/listeners.db') == False:
-        os.mkdir('/opt/gs-payload')
+    if os.path.exists('~/.gs-payload/listeners.db') == False:
+        os.mkdir('~/.gs-payload')
         db.db_create()
     else:
         pass
