608ad57953c188b6116c4bc9e3ecb8b8487ae87c
secw01f <16842677+secw01f@users.noreply.github.com>
2021-09-20 15:06:22 -0400

    updated directory

--- gs-payload.py
+++ gs-payload.py
@@ -79,8 +79,8 @@ def main():
         elif o in ('-u', '--url'):
             Url = a
 
-    if os.path.exists('/usr/local/gs-payload/listeners.db') == False:
-        os.mkdir('/usr/local/gs-payload')
+    if os.path.exists('/opt/gs-payload/listeners.db') == False:
+        os.mkdir('/opt/gs-payload')
         db.db_create()
     else:
         pass
